552447ca35c8967278d616ed7700d20bf29127ac
Thiago <thiago_boimer@hotmail.com>
2022-10-17 14:28:45 -0300

    Some refactoring and better description

--- ServiceName.py
+++ ServiceName.py
@@ -13,6 +13,7 @@ logging.basicConfig(filename='C:\PATH\service.log', format='[%(asctime)s] | %(le
 
 class ServiceName(win32serviceutil.ServiceFramework):
 
+    _exe_name_ = "Name of .exe file"
     _svc_name_ = "Name that will show under de service tree on Task Manager" # Note: the .exe has the name of the  .py file, this doesn't change it
     _svc_display_name_ = "Name that will show on Services window"
     _svc_description_ = "Description on Services window"
@@ -20,30 +21,45 @@ class ServiceName(win32serviceutil.ServiceFramework):
     # Constructor of the windows service
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
-        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
-        socket.setdefaulttimeout(60)
+        # self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        # socket.setdefaulttimeout(60)
 
-    # Called when the service is asked to start
     def SvcDoRun(self):
+        """
+        Called when the service is asked to start
+        """
+        
+        # Logs service start to Event Viewer
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
                               servicemanager.PYS_SERVICE_STARTED,
                               (self._svc_name_, ''))
+        
+        # Sets running to True (just so we can use it in main)
         self.isrunning = True
 
         # Start main according to schedule
         main_trigger.start_rightTime(self.main)
     
-    # Called when the service is asked to stop
     def SvcStop(self):
-        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        """
+        Called when the service is asked to stop
+        """ 
+        # Logs service stop to Event Viewer
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STOPPED,
+                              (self._svc_name_, ''))
+        
+        # Sets running to False (just so we can use it in main)
         self.isrunning = False
-        win32event.SetEvent(self.hWaitStop)
+        
+        # win32event.SetEvent(self.hWaitStop)
 
-    # Main function (run logic here)
     def main(self):
-
+        """
+        Service main logic here
+        """
         try:
-            # This will keep your service running
+            # This will keep service running
             while self.isrunning:           
 
                 # Sleep 5 min then run again (example)
@@ -51,8 +67,10 @@ class ServiceName(win32serviceutil.ServiceFramework):
         except Exception as e:
             logging.info(str(e))
 
-# Checks if module is main and sys args to handle with command prompt functionalities (install, update, remove)
+# __main__ block 
 if __name__ == '__main__':
+    
+    # sys args to handle with command prompt functionalities (install, update, remove)
     if len(sys.argv) == 1:
         servicemanager.Initialize()
         servicemanager.PrepareToHostSingle(ServiceName)
